15b09089594c7eafa39a910aa11741399f58fbc2
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-13 12:44:18 +0100

    added words

---
+++ ex14/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    //Only change code below this line
+var myNoun = "dog";
+var myVerb = "ran";
+var myAdjective = "big";
+var myAdverb = "quickly";
+var wordBlanks = "big" + " " + "dog" + " " +"run" + " " + "quickly" + ".";
+
+
+
+   
+return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
+
