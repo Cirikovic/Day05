f36e9ea4aa914bdd1c6d98a60ac208aadb0db9fc
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-13 11:31:29 +0100

    + myName

---
+++ ex06/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    //Only change code below this line
+var myName = "Almir";
+var myString = "My name is " + myName + " and i am well!"
+
+   
+return {
+        myString
+    };
+}
+
+console.log(main());
+module.exports = main;
+
