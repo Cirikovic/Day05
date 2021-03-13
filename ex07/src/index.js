880bd76e287c643b12798222e39c53bb7e25efd2
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-13 11:38:30 +0100

    again string +=

---
+++ ex07/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    //Only change code below this line
+var someAdjective = "the best!";
+var myString = "Mistral is ";
+myString += someAdjective;
+
+   
+return {
+        myString
+    };
+}
+
+console.log(main());
+module.exports = main;
+
