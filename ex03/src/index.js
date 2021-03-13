3bee1a21eed5a7e7e1c90eab803bf7eec1e0c6a5
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-13 11:10:28 +0100

    C:/Program Files/Git/string quote

---
+++ ex03/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //Only change code below this line
+var myString = "FirstLine\n\tSecondLine\nThirdLine"
+   
+return {
+        myString
+    };
+}
+
+console.log(main());
+module.exports = main;
+
