65a58da48c424d0f3d0539dfaedb24a0c5d0ba32
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-13 12:17:59 +0100

    last letter.length -1

---
+++ ex12/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    //Only change code below this line
+var lastName= "Tesla";
+var lastLetterOfLastName = lastName[lastName.length -1];
+
+
+   
+return {
+    lastLetterOfLastName
+    };
+}
+
+console.log(main());
+module.exports = main;
+
