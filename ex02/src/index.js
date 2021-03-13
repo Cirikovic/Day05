f758323058ffe2d0b135ff7a79a72a3d4d3d18e6
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-13 10:50:02 +0100

    novo js

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //Only change code below this line
+var myString = "FirstLine"\n\t"SecondLine\n"ThirdLine"
+   
+return {
+        myString
+    };
+}
+
+console.log(main());
+module.exports = main;
+
