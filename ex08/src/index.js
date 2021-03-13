b44853ff5a760a8a3a8c458ebc529744f89db102
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-13 11:53:13 +0100

    .lenght

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    //Only change code below this line
+var lastName = "Doe";
+var lastNameLength = "Doe".length;
+
+
+   
+return {
+        lastNameLength
+    };
+}
+
+console.log(main());
+module.exports = main;
+
