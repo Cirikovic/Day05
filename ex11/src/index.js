871b1065579c904a769ffb19a44c308fac24a9b7
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-13 12:12:48 +0100

    thirdletter

---
+++ ex11/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    //Only change code below this line
+var lastName= "Tesla";
+var thirdLetterOfLastName = lastName[2];
+
+
+   
+return {
+    thirdLetterOfLastName
+    };
+}
+
+console.log(main());
+module.exports = main;
+
